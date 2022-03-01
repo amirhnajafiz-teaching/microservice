bfe58d3ce4ee48b02139694bcfaadafaf8116046
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-01 12:56:04 +0330

    cmd service init

---
+++ internal/cmd/server.go
@@ -0,0 +1,10 @@
+package cmd
+
+// StringService provides operations on strings.
+type StringService interface {
+	Uppercase(string) (string, error)
+	Lowercase(string) (string, error)
+	Concat(string, string) (string, error)
+	Split(string, string) ([]string, error)
+	Count(string) int
+}
