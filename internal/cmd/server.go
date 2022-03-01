2aae23a214db97999dfb30c197a427b4ce049c63
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-01 13:02:23 +0330

    handlers for uppercase and count added'

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -1,10 +1,1 @@
 package cmd
-
-// StringService provides operations on strings.
-type StringService interface {
-	Uppercase(string) (string, error)
-	Lowercase(string) (string, error)
-	Concat(string, string) (string, error)
-	Split(string, string) ([]string, error)
-	Count(string) int
-}
--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,4 +1,23 @@
 package handler
 
+import (
+	"errors"
+	"strings"
+)
+
+// ErrEmpty is returned when an input string is empty.
+var ErrEmpty = errors.New("empty string")
+
 // Handler is a concrete implementation of StringService
 type Handler struct{}
+
+func (Handler) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (Handler) Count(s string) int {
+	return len(s)
+}
