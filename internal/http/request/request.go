b36ec43e7787d23bd4fa445e84303cb3a8b2af8d
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-01 13:10:21 +0330

    all requests added

--- internal/http/request/request.go
+++ internal/http/request/request.go
@@ -6,6 +6,20 @@ type UppercaseRequest struct {
 	S string `json:"s"`
 }
 
+type LowercaseRequest struct {
+	S string `json:"s"`
+}
+
+type ConcatenateRequest struct {
+	S string `json:"s"`
+	C string `json:"c"`
+}
+
+type SplitRequest struct {
+	S string `json:"s"`
+	K string `json:"k"`
+}
+
 type CountRequest struct {
 	S string `json:"s"`
 }
