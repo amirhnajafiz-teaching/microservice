6e8c4f18811e176a6e5f07bdbdc017f5e74bdce6
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-01 13:11:40 +0330

    all responses added

--- internal/http/response/response.go
+++ internal/http/response/response.go
@@ -7,6 +7,21 @@ type UppercaseResponse struct {
 	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
 }
 
+type LowercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
+type ConcatenateResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
+type SplitResponse struct {
+	V   []string `json:"v"`
+	Err string   `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
 type CountResponse struct {
 	V int `json:"v"`
 }
