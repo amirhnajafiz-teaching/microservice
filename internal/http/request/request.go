dfd004d5e949f9e2e6925b620c9e3595e6c4b1b9
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-01 13:08:51 +0330

    two basic handlers request response added

--- internal/http/request/request.go
+++ internal/http/request/request.go
@@ -1,1 +1,11 @@
 package request
+
+// For each method, we define request structs
+
+type UppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type CountRequest struct {
+	S string `json:"s"`
+}
--- internal/http/response/response.go
+++ internal/http/response/response.go
@@ -1,1 +1,12 @@
 package response
+
+// For each method, we define response structs
+
+type UppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
+type CountResponse struct {
+	V int `json:"v"`
+}
